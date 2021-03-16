5978f4bd93c07bf32fbc53524df91d40d2b8ca01
Romulus Crisan <cromica@gmail.com>
2021-03-16 13:54:01 +0200

    update push url

--- releaseDocs.sh
+++ releaseDocs.sh
@@ -30,4 +30,4 @@ git config --local user.email "github-actions[bot]@users.noreply.sdl.com"
 git config --local user.name "github-actions[bot]"
 git add . -A
 git commit -m "Update generated documentation"
-git push origin "${remote_repo}" HEAD:gh-pages
+git push "${remote_repo}" HEAD:gh-pages
