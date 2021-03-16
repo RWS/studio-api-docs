059266431110293091b6beb1ed0da6c2a080e591
Romulus Crisan <cromica@gmail.com>
2021-03-16 13:21:29 +0200

    Update releaseDocs.sh
    added authentication token

--- releaseDocs.sh
+++ releaseDocs.sh
@@ -3,16 +3,16 @@ set -e
 
 echo "Build the documentation site" 
 
-
 SOURCE_DIR=$PWD
 TEMP_REPO_DIR=$PWD/../docs-gh-pages
+remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/sdl/studio-api-docs.git"
 
 echo "Removing temporary doc directory $TEMP_REPO_DIR"
 rm -rf $TEMP_REPO_DIR
 mkdir $TEMP_REPO_DIR
 
 echo "Cloning the repo with the gh-pages branch"
-git clone https://github.com/sdl/studio-api-docs.git --branch gh-pages $TEMP_REPO_DIR
+git clone remote_repo --branch gh-pages $TEMP_REPO_DIR
 
 echo "Clear repo directory"
 cd $TEMP_REPO_DIR
