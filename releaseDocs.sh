b1a17dd494e2e9da925057d28ed167956653c518
Romulus Crisan <cromica@gmail.com>
2021-03-16 13:50:58 +0200

    call with the right param values

--- releaseDocs.sh
+++ releaseDocs.sh
@@ -6,7 +6,10 @@ echo "Build the documentation site"
 SOURCE_DIR=$PWD
 TEMP_REPO_DIR=$PWD/../docs-gh-pages
 
-remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/sdl/studio-api-docs.git"
+ACTOR=$1
+TOKEN=$2
+
+remote_repo="https://${ACTOR}:${TOKEN}@github.com/sdl/studio-api-docs.git"
 
 echo "Removing temporary doc directory $TEMP_REPO_DIR"
 rm -rf $TEMP_REPO_DIR
