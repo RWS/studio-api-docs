bb4a2627f423278fd502a13cba431d8337b4e58f
Romulus Crisan <cromica@gmail.com>
2021-03-16 12:33:54 +0200

    Create releaseDocs.sh
    add script for publish to gh-pages branch

---
+++ releaseDocs.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+set -e
+
+echo "Build the documentation site" 
+
+
+SOURCE_DIR=$PWD
+TEMP_REPO_DIR=$PWD/../docs-gh-pages
+
+echo "Removing temporary doc directory $TEMP_REPO_DIR"
+rm -rf $TEMP_REPO_DIR
+mkdir $TEMP_REPO_DIR
+
+echo "Cloning the repo with the gh-pages branch"
+git clone https://github.com/sdl/studio-api-docs.git --branch gh-pages $TEMP_REPO_DIR
+
+echo "Clear repo directory"
+cd $TEMP_REPO_DIR
+git rm -r *
+
+echo "Copy documentation into the repo"
+cp -r $SOURCE_DIR/_site/* .
+
+echo "Push the new docs to the remote branch"
+git add . -A
+git commit -m "Update generated documentation"
+git push origin gh-pages
