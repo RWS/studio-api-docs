e17edd807c23f99ac82d0461db81f08412a04572
Romulus Crisan <cromica@gmail.com>
2021-03-16 13:35:58 +0200

    added proper user

--- releaseDocs.sh
+++ releaseDocs.sh
@@ -6,7 +6,7 @@ echo "Build the documentation site"
 SOURCE_DIR=$PWD
 TEMP_REPO_DIR=$PWD/../docs-gh-pages
 remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/sdl/studio-api-docs.git"
-
+echo "Remote repo is "
 echo "Removing temporary doc directory $TEMP_REPO_DIR"
 rm -rf $TEMP_REPO_DIR
 mkdir $TEMP_REPO_DIR
@@ -22,8 +22,8 @@ echo "Copy documentation into the repo"
 cp -r $SOURCE_DIR/articles/* .
 
 echo "Push the new docs to the remote branch"
-git config --local user.email "github-actions[bot]@users.noreply.sdl.com"
-git config --local user.name "github-actions[bot]"
+git config --local user.email "cromica@gmail.com"
+git config --local user.name "Romulus Crisan"
 git add . -A
 git commit -m "Update generated documentation"
 git push origin gh-pages
