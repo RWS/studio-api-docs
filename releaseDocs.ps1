6dc6e6a3407e3294e8c0633731d86d2dc3193d30
Ionel-Cristian Moldovan <66057010+icmoldovan@users.noreply.github.com>
2021-11-15 15:46:04 +0200

    Update releaseDocs.ps1

--- releaseDocs.ps1
+++ releaseDocs.ps1
@@ -11,24 +11,16 @@ $TEMP_REPO_DIR=[System.IO.Path]::GetFullPath("$psscriptroot/../docs-gh-pages")
 
 $remote_repo="https://github-actions:${TOKEN}@github.com/sdl/studio-api-docs.git"
 
-write-host "Removing temporary doc directory $TEMP_REPO_DIR"
-Remove-Item $TEMP_REPO_DIR -Force -Recurse -ErrorAction Ignore
-mkdir $TEMP_REPO_DIR
-
-write-host "Cloning the repo $remote_repo with the gh-pages-vers_test branch"
-git clone $remote_repo --branch gh-pages-vers_test $TEMP_REPO_DIR
-
-#write-host "Clear repo directory"
-#Set-Location $TEMP_REPO_DIR
-#git rm -r *
-
+write-host "Cloning the repo $remote_repo with the gh-pages branch"
+git clone $remote_repo --branch gh-pages $TEMP_REPO_DIR
+cd $TEMP_REPO_DIR
 write-host "Copy documentation into the repo"
-Copy-Item "$SOURCE_DIR\_site\*" . -Recurse -force
+mkdir "16.1"
+Copy-Item "$SOURCE_DIR\_site\16.1\*" .\16.1\ -Recurse -force
 
 write-host "Push the new docs to the remote branch"
 git config --local user.email "github-actions[bot]@users.noreply.sdl.com"
 git config --local user.name "github-actions[bot]"
-git add . -A
+git add .\16.1 -A
 git commit -m "Update generated documentation"
-
-git push "$remote_repo" HEAD:gh-pages-vers_test
+git push "$remote_repo" HEAD:gh-pages
