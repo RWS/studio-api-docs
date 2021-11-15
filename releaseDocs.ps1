492f6932a7c0a2b005055e15cd90e87804b86b26
Ionel-Cristian Moldovan <66057010+icmoldovan@users.noreply.github.com>
2021-11-15 15:23:31 +0200

    Update releaseDocs.ps1

--- releaseDocs.ps1
+++ releaseDocs.ps1
@@ -23,5 +23,4 @@ git config --local user.email "github-actions[bot]@users.noreply.sdl.com"
 git config --local user.name "github-actions[bot]"
 git add .\15.2 -A
 git commit -m "Update generated documentation"
-git pull
-git push "$remote_repo" HEAD:gh-pages
+git push "$remote_repo" HEAD:gh-pages --no-verify
