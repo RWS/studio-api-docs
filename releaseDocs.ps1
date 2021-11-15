75fd58da0a8038b230fd05ba543707666a898e39
Ionel-Cristian Moldovan <66057010+icmoldovan@users.noreply.github.com>
2021-11-15 15:40:23 +0200

    Update releaseDocs.ps1

--- releaseDocs.ps1
+++ releaseDocs.ps1
@@ -13,7 +13,7 @@ $remote_repo="https://github-actions:${TOKEN}@github.com/sdl/studio-api-docs.git
 
 write-host "Cloning the repo $remote_repo with the gh-pages branch"
 git clone $remote_repo --branch gh-pages $TEMP_REPO_DIR
-
+cd $TEMP_REPO_DIR
 write-host "Copy documentation into the repo"
 mkdir "15.2"
 Copy-Item "$SOURCE_DIR\_site\15.2\*" .\15.2\ -Recurse -force
@@ -23,4 +23,4 @@ git config --local user.email "github-actions[bot]@users.noreply.sdl.com"
 git config --local user.name "github-actions[bot]"
 git add .\15.2 -A
 git commit -m "Update generated documentation"
-git push "$remote_repo" --no-verify
+git push "$remote_repo" HEAD:gh-pages
