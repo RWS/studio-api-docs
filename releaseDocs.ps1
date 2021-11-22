773f78847f87be8ed5b5b7e6f664d1c911f9efda
imoldovan@sdl.com <icmoldovan>
2021-11-22 16:50:52 +0200

    update workflow

--- releaseDocs.ps1
+++ releaseDocs.ps1
@@ -14,6 +14,7 @@ $remote_repo="https://github-actions:${TOKEN}@github.com/sdl/studio-api-docs.git
 write-host "Cloning the repo $remote_repo with the gh-pages branch"
 git clone $remote_repo --branch gh-pages $TEMP_REPO_DIR
 Set-Location $TEMP_REPO_DIR
+git checkout -b gh-pages_new
 write-host "Copy documentation into the repo"
 
 Copy-Item "$SOURCE_DIR\_site\*" .\ -Recurse -force
@@ -23,4 +24,6 @@ git config --local user.email "github-actions[bot]@users.noreply.sdl.com"
 git config --local user.name "github-actions[bot]"
 git add .\ -A
 git commit -m "Update generated documentation"
-git push "$remote_repo" HEAD:gh-pages
+git push "$remote_repo" HEAD:gh-pages_new
+gh pr create --title "Update generated documentation" --body "Update generated documentation" -B gh-pages
+
